e9bcfa40f3cb2cc6af239325c776594bbdd3f495
eunji98 <eunji98@kissoft.co.kr>
2019-10-31 10:14:41 +0900

    c

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SlideLibrary",
-            dependencies: []),
+                path: "Sources",
+                exclude: [],
+                sources: ["SlideLibrary/AMSlideMenuContentSegue.m"],
+                publicHeadersPath: "SlideLibrary"),
         .testTarget(
             name: "SlideLibraryTests",
             dependencies: ["SlideLibrary"]),
